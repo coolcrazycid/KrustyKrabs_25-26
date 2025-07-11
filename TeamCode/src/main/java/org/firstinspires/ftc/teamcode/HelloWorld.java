69d26d59d733d7ef712b40f30102c5ed63f68010
Charik Malik <charikmalik@outlook.com>
2025-07-10 20:55:45 -0700

    Finished HelloWorld.java as an initial test and created PrimitiveTypes.java to work on datatypes from java booklet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Charik");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
