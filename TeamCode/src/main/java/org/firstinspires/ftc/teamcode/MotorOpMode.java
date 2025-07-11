6c66498c7b43c75d9b0684f03114416b81a69bee
Charik Malik <charikmalik@outlook.com>
2025-07-11 11:44:53 -0700

    fixed error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class MotorOpMode extends OpMode {
-    ProgrammingBoard4 board = newProgrammingBoard4();
+    ProgrammingBoard4 board = new ProgrammingBoard4();
     @Override
     public void init() {
             board.init(hardwareMap);
